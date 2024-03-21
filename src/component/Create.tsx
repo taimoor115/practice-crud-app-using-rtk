35817b24c018b33bcf2af9454545d6f41b5725bc
Taimoor Hussain <taimoorhussain985@gmail.com>
2024-03-21 23:26:41 +0500

    create operation

--- src/component/Create.tsx
+++ src/component/Create.tsx
@@ -1,42 +1,83 @@
-const Create = () => {
-  return (
-    <form className="w-50 mx-auto my-5">
-      <div className="mb-3">
-        <label className="form-label">Name</label>
-        <input type="text" name="name" className="form-control" required />
-      </div>
-      <div className="mb-3">
-        <label className="form-label">Email</label>
-        <input type="email" name="email" className="form-control" required />
-      </div>
-      <div className="mb-3">
-        <label className="form-label">Age</label>
-        <input type="text" name="age" className="form-control" required />
-      </div>
-      <div className="mb-3">
-        <input
-          className="form-check-input"
-          name="gender"
-          value="Male"
-          type="radio"
-          required
-        />
-        <label className="form-check-label">Male</label>
-      </div>
-      <div className="mb-3">
-        <input
-          className="form-check-input"
-          name="gender"
-          value="Female"
-          type="radio"
-        />
-        <label className="form-check-label">Female</label>
-      </div>
+import { FormEvent, useState } from "react";
+import { useDispatch } from "react-redux";
+import { createUser } from "../features/userDetailSlice";
 
-      <button type="submit" className="btn btn-primary">
-        Submit
-      </button>
-    </form>
+const Create = () => {
+  const [users, setUsers] = useState({});
+  const dispatch = useDispatch();
+
+  const getUserData = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setUsers({ ...users, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = (e: FormEvent) => {
+    e.preventDefault();
+    console.log(users);
+    dispatch(createUser(users));
+  };
+
+  return (
+    <>
+      <h3 className="text-center text-primary mt-2">Fill the Fields</h3>
+
+      <form className="w-50 mx-auto my-5 text-primary" onSubmit={handleSubmit}>
+        <div className="mb-3">
+          <label className="form-label">Name</label>
+          <input
+            type="text"
+            name="name"
+            className="form-control"
+            required
+            onChange={getUserData}
+          />
+        </div>
+        <div className="mb-3">
+          <label className="form-label">Email</label>
+          <input
+            type="email"
+            name="email"
+            className="form-control"
+            required
+            onChange={getUserData}
+          />
+        </div>
+        <div className="mb-3">
+          <label className="form-label">Age</label>
+          <input
+            type="text"
+            name="age"
+            className="form-control"
+            required
+            onChange={getUserData}
+          />
+        </div>
+        <div className="mb-3">
+          <input
+            className="form-check-input"
+            name="gender"
+            value="Male"
+            type="radio"
+            onChange={getUserData}
+            required
+          />
+          <label className="form-check-label ms-2">Male</label>
+        </div>
+        <div className="mb-3">
+          <input
+            className="form-check-input"
+            name="gender"
+            value="Female"
+            onChange={getUserData}
+            type="radio"
+          />
+          <label className="form-check-label ms-2">Female</label>
+        </div>
+
+        <button type="submit" className="btn btn-primary">
+          Submit
+        </button>
+      </form>
+    </>
   );
 };
 
--- src/features/userDetailSlice.ts
+++ src/features/userDetailSlice.ts
@@ -1,13 +1,65 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+
+interface User {
+  id: number;
+  name: string;
+  email: string;
+  age: number;
+  gender: string;
+}
+
+interface UserState {
+  users: User[];
+  loading: boolean;
+  error: string | undefined;
+}
+
+const initialState: UserState = {
+  users: [],
+  loading: false,
+  error: undefined,
+};
 
 const userDetailSlice = createSlice({
   name: "userDetail",
-  initialState: {
-    users: [],
-    loading: false,
-    error: null,
-  },
+  initialState,
   reducers: {},
+  extraReducers: (builder) => {
+    builder
+      .addCase(createUser.pending, (state) => {
+        state.loading = true;
+      })
+      .addCase(createUser.fulfilled, (state, action) => {
+        state.loading = false;
+        state.users.push(action.payload);
+      })
+      .addCase(createUser.rejected, (state, action) => {
+        state.loading = false;
+        state.error = action?.error?.message;
+      });
+  },
 });
+
+export const createUser = createAsyncThunk(
+  "createUser",
+  async (data, { rejectWithValue }) => {
+    const res = await fetch(
+      "https://65fa8ab83909a9a65b1aa217.mockapi.io/Crud",
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(data),
+      }
+    );
+    try {
+      const data = await res.json();
+      return data;
+    } catch (error) {
+      return rejectWithValue(error);
+    }
+  }
+);
 
 export default userDetailSlice.reducer;
